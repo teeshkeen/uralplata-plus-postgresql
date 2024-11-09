d88ce86837143a46eb2bbffdb52d9aea869b54d4
teeshkeen <lxtishkin@gmail.com>
2024-11-09 18:39:49 +0500

    fixes catalog button

--- frontend/src/shared/ui/Button.jsx
+++ frontend/src/shared/ui/Button.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Button = ({text, href, styles, onClick}) => {
   return (
-    <a onClick={onClick} href={href} className={`flex justify-center ButtonFormSubmit cursor-pointer px-10 py-3 rounded-[10px] bg-clickable-default hover:bg-clickable-hover active:bg-clickable-active transition-all text-base font-normal text-white 768:inline-block 768:text-xl ${styles}`}>
+    <a onClick={onClick} href={href} className={`flex justify-center ButtonFormSubmit cursor-pointer px-10 py-3 rounded-[10px]
+       bg-clickable-default hover:bg-clickable-hover active:bg-clickable-active transition-all text-base font-normal
+        text-white 768:inline-block text-[16px] 768:text-[18px]
+     ${styles}`}>
       <div className="text-center">{text}</div>
       </a>
   )
